a4f1a3c42e143c542a07178c62eacbf869ecab67
Rafa <rahersi@campusaula.com>
2018-11-14 20:12:56 +0100

    Hola mundo > Hola mundillo

--- src/ede_dummy2/EDE_dummy2.java
+++ src/ede_dummy2/EDE_dummy2.java
@@ -15,7 +15,7 @@ public class EDE_dummy2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hola mundo");
+        System.out.print("Hola mundillo");
     }
     
 }
